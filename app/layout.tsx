a09a4779513930be0a872be70283fa6a3d275f56
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2023-02-22 20:34:09 -0500

    feat(footer): add link to old site

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { Work_Sans } from "@next/font/google";
 import Anchor from "@/components/Anchor";
 import "./globals.css";
 
+const SPOTLIGHT_LINK = "https://spotlight-i7ara7c2z-zacowan.vercel.app/";
+
 const workSans = Work_Sans({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -19,10 +21,13 @@ export default function RootLayout({
       <body>
         {children}
         <footer className="flex justify-center px-3 py-20 border-t border-neutral-500 mt-20">
-          <div className="max-w-prose w-full space-y-8">
+          <div className="max-w-prose w-full space-y-4">
             <p className="block text-xs text-neutral-300">
               Designed and engineered by Zachary Cowan.
             </p>
+            <Anchor className="block text-xs" href={SPOTLIGHT_LINK}>
+              Old Site
+            </Anchor>
           </div>
         </footer>
       </body>
